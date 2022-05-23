944964136ced615bc36915f1e48a879f22a4557d
Nguyễn Minh Tuấn <tuannb0mmm.2x1y9z@Gmail.cokm>
2022-05-23 10:56:07 +0700

    WIP: home page

---
+++ home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home-info</title>
+    <!-- custom css file style -->
+    <link rel="stylesheet" href="./Style.css">
+</head>
+<body>
+    <div id="filter"></div>
+    <div class="container">
+        <div class="profile">
+            <div class="user-image">
+
+            </div>
+            <h3 class="username">
+
+            </h3>
+            <div class="user-info">
+                <span class="userFullName">
+
+                </span>
+                <span class="userEmail">
+
+                </span>
+                <span class="userPhoneNumber">\
+
+                </span>
+            </div>
+            <div class="update info">
+                <a href="./updateInfo.php" class="btnUpdate">Update Profile</a>
+            </div>
+            <div class="logout">
+                <a href="./login.php" class="btnLogout">Logout</a>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
